99584c551654e994794319858c1c0a54425c0b0c
Taras Venger <taras-venger@users.noreply.github.com>
2019-10-07 16:04:43 +0300

    Create getPieces.js

---
+++ src/store/utility/getPieces.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Piece from '../../components/Piece';
+
+export const loadImage = url => {
+  return new Promise((resolve, reject) => {
+    const image = document.createElement('img');
+    image.crossOrigin = 'anonymous';
+    image.src = url;
+    image.onload = () => resolve(image);
+  });
+};
+
+export const cutImage = (
+  image,
+  imgWidth,
+  imgHeight,
+  numberOfRows,
+  numberOfColumns
+) => {
+  let pieces = [];
+  for (let y = 0; y < numberOfRows; y++) {
+    for (let x = 0; x < numberOfColumns; x++) {
+      const piece = document.createElement('canvas');
+      const context = piece.getContext('2d');
+      piece.width = imgWidth / numberOfColumns; //rounds down
+      piece.height = imgHeight / numberOfRows; //rounds down
+      context.drawImage(
+        image,
+        x * piece.width,
+        y * piece.height,
+        piece.width,
+        piece.height,
+        0,
+        0,
+        piece.width,
+        piece.height
+      );
+      pieces.push({
+        width: piece.width,
+        height: piece.height,
+        url: piece.toDataURL()
+      });
+    }
+  }
+  return pieces;
+};
+
+export const transformPieces = pieces =>
+  pieces.map((piece, index) => {
+    const { width, height, url } = piece;
+    return (
+      <Piece key={index} id={index} width={width} height={height} url={url} />
+    );
+  });
